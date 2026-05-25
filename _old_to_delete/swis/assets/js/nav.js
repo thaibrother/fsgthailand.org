a0ce51508e1cff32e2b690db1507624204b4a34d
Bro.Book <supanun@yahoo.com>
2026-05-25 15:10:19 +0700

    backup and uplaad

--- _old_to_delete/swis/assets/js/nav.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(){
-  var burger=document.querySelector('.nav .burger');
-  var links=document.querySelector('.nav .links');
-  if(burger&&links){
-    burger.addEventListener('click',function(){
-      links.classList.toggle('open');
-    });
-  }
-});
